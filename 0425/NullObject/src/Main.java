f9c107a1e530b69bc065670036dde0997d7c14ef
R10725045 <r10725045@ntu.edu.tw>
2023-04-22 14:49:37 +0800

    add test case

--- 0425/NullObject/src/Main.java
+++ 0425/NullObject/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
-    public static void main(String[] args) {
+    public static String main(String[] args) {
         System.out.println("Hello world!");
+        return "Hello world!";
     }
 }
