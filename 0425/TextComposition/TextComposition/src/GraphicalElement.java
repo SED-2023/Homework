40e53af415f863ccd152c2e3f84ee3b8143dd93a
r10725045@ntu.edu.tw <r10725045@ntu.edu.tw>
2023-04-22 17:33:11 +0800

    Constructor

--- 0425/TextComposition/TextComposition/src/GraphicalElement.java
+++ 0425/TextComposition/TextComposition/src/GraphicalElement.java
@@ -4,4 +4,11 @@ public class GraphicalElement extends Component {
     protected int shrinkAbility;
     protected int stretchAbility;
     protected String content;
+    public GraphicalElement(int ID, int size, int shrinkAbility, int stretchAbility, String content){
+        this.componentID = ID;
+        this.size = size;
+        this.shrinkAbility = shrinkAbility;
+        this.stretchAbility = stretchAbility;
+        this.content = content;
+    }
 }
