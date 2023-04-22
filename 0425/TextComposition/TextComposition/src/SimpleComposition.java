10ed29b131a4fecebe39f2e596ef7997ce6fdcd3
BOBO-LU <luwenkai0820@gmail.com>
2023-04-22 16:41:55 +0800

    add simpleComposition

--- 0425/TextComposition/TextComposition/src/SimpleComposition.java
+++ 0425/TextComposition/TextComposition/src/SimpleComposition.java
@@ -1,10 +1,19 @@
-import java.awt.*;
+
 import java.util.ArrayList;
 
 public class SimpleComposition implements LinebreakingStrategy{
     @Override
     public ArrayList<ArrayList<Component>> linebreak(ArrayList<Component> components) {
-        ArrayList<ArrayList<Component>> result = new ArrayList<ArrayList<Integer> >(0);
-        return null;
+        ArrayList<ArrayList<Component>> result =
+                new ArrayList<ArrayList<Component> >(0);
+
+        for (int i = 0; i < components.size(); i++) {
+            ArrayList<Component> oneline = new ArrayList<Component>(0);
+            oneline.add(components.get(i));
+            result.add(oneline)
+        }
+
+
+        return result;
     }
 }
