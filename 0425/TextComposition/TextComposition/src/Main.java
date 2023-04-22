ece9e50c74558151546aad44e1a366c904806ffb
r10725045@ntu.edu.tw <r10725045@ntu.edu.tw>
2023-04-22 18:07:35 +0800

    Main

--- 0425/TextComposition/TextComposition/src/Main.java
+++ 0425/TextComposition/TextComposition/src/Main.java
@@ -1,6 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        input = inputParser();
-        output = client(input);
+        Client client = new Client();
+
+        try {
+            InputParser input = new InputParser();
+            BufferedReader fileReader = input.parse(args);
+            String line = fileReader.readLine();
+            while (line != null) {
+                String[] tokens = line.split(" ");
+                String command = tokens[0];
+                switch (command) {
+                    case "Text":
+                    case "GraphicalElement":{
+                        client.newComponent(tokens);
+                    }
+                    case "ChangeSize":{
+                        client.changeSize(tokens);
+                    }
+                    case "Require":{
+                        client.require(tokens);
+                    }
+                }
+            }
+        } catch (IOException ex) {
+            System.out.println("Input Error");
+        }
     }
 }
