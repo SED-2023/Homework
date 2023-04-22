5a6f9dbb47833f06c7473eac3c8ea36bfed7046e
BOBO-LU <luwenkai0820@gmail.com>
2023-04-22 17:07:02 +0800

    add ArrayComposition

--- 0425/TextComposition/TextComposition/src/ArrayComposition.java
+++ 0425/TextComposition/TextComposition/src/ArrayComposition.java
@@ -4,7 +4,25 @@ import java.util.ArrayList;
 public class ArrayComposition implements LinebreakingStrategy {
     @Override
     public ArrayList<ArrayList<Component>> linebreak(ArrayList<Component> components) {
+        int lineSize = 3;
+        ArrayList<ArrayList<Component>> result =
+                new ArrayList<ArrayList<Component> >(0);
+        ArrayList<Component> oneLine = new ArrayList<Component>(0);
 
-        return null;
+        for (int i = 0; i < components.size(); i++) {
+
+            if (i % lineSize == 0) {
+                // if reach lineSize, append oneLine into result
+                result.add(oneLine);
+                oneLine.clear();
+            }else {
+                // else, add component to oneLine
+                oneLine.add(components.get(i));
+            }
+        }
+        if (oneLine.size() != 0) {
+            result.add(oneLine);
+        }
+        return result;
     }
 }
