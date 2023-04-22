e3fd5eb51e3014851af16097966be2d4f2a845c8
R10725045 <r10725045@ntu.edu.tw>
2023-04-22 17:40:13 +0800

    add test case

--- 0425/NullObject/src/MainTest.java
+++ 0425/NullObject/src/MainTest.java
@@ -14,16 +14,42 @@ import java.util.Date;
 class MainTest {
 
     static String[] args;
+    static String path = System.getProperty("user.dir");
+
     @BeforeEach
-    void setup(){
+    void setup() throws IOException {
         this.args = new String[5];
     }
 
-//    @Test
-//    void print() {
-//        Main m = new Main();
-//        assertEquals("Hello world!", m.main(args));
-//    }
+    @Test
+    void testCase1() throws IOException {
+        String inputFile = path + "\\sampleInput.in";
+        String outputFile = path + "\\sampleOutput.out";
+        System.out.println(inputFile);
+
+        try (BufferedReader inputReader = new BufferedReader(new FileReader(inputFile));
+             BufferedReader outputReader = new BufferedReader(new FileReader(outputFile))) {
+
+            String inputLine = inputReader.readLine();
+            String outputLine = outputReader.readLine();
+            String outputStr = "";
+
+            while (inputLine != null) {
+                inputLine = inputReader.readLine();
+            }
+
+            while (outputLine != null) {
+                outputStr += outputLine;
+                outputLine = outputReader.readLine();
+            }
+
+            Main m = new Main();
+            System.out.println(outputStr);
+//            assertEquals("Hello world!", m.main(args));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 //
 //    @Test
 //    void print2() {
