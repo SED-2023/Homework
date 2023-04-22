1e9ed38f33753c331e3e5558f771ae8642aacbcb
R10725045 <r10725045@ntu.edu.tw>
2023-04-22 14:35:19 +0800

    init

---
+++ 0425/NullObject/src/MainTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+    @Test
+    void print() {
+        Main m = new Main();
+//        assertEquals("Hello Laura!", m.print());
+    }
+
+}
