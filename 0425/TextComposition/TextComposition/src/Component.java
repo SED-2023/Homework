98aa14bd7e4cbc4b70824617353e04b1c9567dec
r10725045@ntu.edu.tw <r10725045@ntu.edu.tw>
2023-04-22 16:40:24 +0800

    Add Component Fixed

--- 0425/TextComposition/TextComposition/src/Component.java
+++ 0425/TextComposition/TextComposition/src/Component.java
@@ -1,7 +1,7 @@
 public interface Component {
-    int componentID;
-    float naturalSize;
-    float shrinkAbility;
-    float stretchAbility;
-    String content;
+    int componentID = 0;
+    float naturalSize = 0;
+    float shrinkAbility = 0;
+    float stretchAbility = 0;
+    String content = "";
 }
