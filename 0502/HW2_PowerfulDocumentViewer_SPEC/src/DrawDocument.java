f489916d7dce97a1b9a8abec9d451962937b4a98
r10725045@ntu.edu.tw <r10725045@ntu.edu.tw>
2023-05-05 18:09:16 +0800

    add document class

---
+++ 0502/HW2_PowerfulDocumentViewer_SPEC/src/DrawDocument.java
@@ -0,0 +1,8 @@
+public class DrawDocument implements Document{
+    private String type;
+
+    @Override
+    public String getType() {
+        return type;
+    }
+}
---
+++ 0502/HW2_PowerfulDocumentViewer_SPEC/src/TextDocument.java
@@ -0,0 +1,7 @@
+public class TextDocument implements Document{
+    private String type;
+    @Override
+    public String getType() {
+        return type;
+    }
+}
