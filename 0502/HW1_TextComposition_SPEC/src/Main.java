f37cfdfeb5564497461b22c5219bf839b3d40ac3
R10725045 <r10725045@ntu.edu.tw>
2023-05-05 16:47:06 +0800

    0502 init

---
+++ 0502/HW1_TextComposition_SPEC/src/Main.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Map<String, AbstractObject> people = new HashMap<>();
+        ArrayList<String> outputString = new ArrayList<>();
+
+        try {
+            InputParser input = new InputParser();
+            BufferedReader fileReader = input.parse(args);
+            String line = fileReader.readLine();
+
+            while (line != null) {
+                String[] tokens = line.split(" ");
+                String command = tokens[0];
+                switch (command) {
+                    case "Person":
+                        people = Client.commandPerson(people, tokens[1], tokens[2], tokens[3], tokens[4]);
+                        break;
+
+                    case "Job":
+                        outputString.add(Client.commandJob(people, tokens[1]));
+                        break;
+
+                    case "WeightAverage":
+                        outputString.add(Client.commandWeightAverage(people, tokens));
+                        break;
+
+                    case "WeightSum":
+                        outputString.add(Client.commandWeightSum(people, tokens));
+                        break;
+
+                    case "HeightAverage":
+                        outputString.add(Client.commandHeightAverage(people, tokens));
+                        break;
+
+                    case "HeightSum":
+                        outputString.add(Client.commandHeightSum(people, tokens));
+                        break;
+                    default:
+                        break;
+                }
+                line = fileReader.readLine();
+            }
+            fileReader.close();
+
+            OutputParser OutputParser = new OutputParser();
+            OutputParser.parse(outputString, args[0].replace("Input", "Output"));
+        } catch (IOException ex) {
+//            System.out.println("Input Error");
+        }
+        //print
+//        people.forEach((k, v) -> System.out.println("Key : " + k + " Value : " + v));
+    }
+}
---
+++ 0502/HW2_PowerfulDocumentViewer_SPEC/src/MainTest.java
@@ -0,0 +1,89 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.File;
+
+class MainTest {
+
+    static String[] args;
+    static String path = System.getProperty("user.dir");
+
+    @BeforeEach
+    void setup() throws IOException {
+        this.args = new String[5];
+    }
+
+    @Test
+    void testMain() throws IOException {
+        System.out.println("\nTest case: sample");
+        System.out.println("Result: ");
+        Main m = new Main();
+        m.main(new String[]{path + "\\sampleInput.in"});
+        String expectedFile = path + "\\sampleOutput.out";
+        String actualFile = path + "\\outputFiles\\sampleOutput.in";
+        assertTrue(parseTwoFiles(expectedFile, actualFile));
+    }
+
+    @Test
+    void testAll() throws IOException {
+        File folder = new File(path + "\\testCase");
+        File[] files = folder.listFiles();
+
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile() && file.getName().endsWith("_Input.txt")) {
+                    String prefix = file.getName().replace("_Input.txt", "");
+                    System.out.println("\nTest case: " + prefix);
+                    System.out.println("Result: ");
+
+                    String inputPath = path + "\\testCase\\" + file.getName();
+                    Main m = new Main();
+                    m.main(new String[]{inputPath});
+
+                    String groundTruth = path + "\\testCase\\" + prefix + "_Output.txt";
+                    String functionOutput = path + "\\outputFiles\\" + prefix + "_Output.txt";
+                    assertTrue(parseTwoFiles(groundTruth, functionOutput));
+                }
+            }
+        }
+        assertTrue(true);
+    }
+
+    Boolean parseTwoFiles(String expectedFile, String actualFile) throws IOException {
+        try (BufferedReader expectedReader = new BufferedReader(new FileReader(expectedFile));
+             BufferedReader actualReader = new BufferedReader(new FileReader(actualFile))) {
+
+            String expectedLine = expectedReader.readLine();
+            String actualLine = actualReader.readLine();
+
+            while (expectedLine != null && actualLine != null) {
+                if (!expectedLine.equals(actualLine)) {
+                    System.out.println("\nExpected: " + expectedLine);
+                    System.out.println("Actual  : " + actualLine);
+                    return false;
+                }
+
+                expectedLine = expectedReader.readLine();
+                actualLine = actualReader.readLine();
+            }
+
+            if (expectedLine != null || actualLine != null) {
+                if(expectedLine != null )
+                    System.out.println("\nExpected: " + expectedLine + "\nActual  : <none>");
+                if(actualLine != null )
+                    System.out.println("\nExpected: <none> \nActual  : "+ actualLine);
+                return false;
+            } else {
+                return true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+}
