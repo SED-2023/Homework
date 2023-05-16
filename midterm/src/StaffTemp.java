5d6138b3cf76c73125c65776759e08b5d14982ae
imaoplab <103180018+imaoplab@users.noreply.github.com>
2023-05-16 11:19:03 +0800

    adsad

---
+++ midterm/src/StaffTemp.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class StaffTemp {
+    public String name;
+
+    public void checkOut(String user1, String user2, ArrayList<Integer> bookList){
+        if ( borrowedBooks.size() <= user2.predefinedBorrowBookNumber - user2.borrowedBooks.size()){
+
+        }
+    }
+}
