5c0a339a3ef7d563707d48f92dd6353826ac23ea
r10725045@ntu.edu.tw <r10725045@ntu.edu.tw>
2023-05-16 11:53:24 +0800

    app draft

--- midterm/src/Application.java
+++ midterm/src/Application.java
@@ -1,57 +1,33 @@
 import java.util.ArrayList;
 
 public class Application {
+    private Map<String, User> users;
 
-    //    public  Style style_obj;
-    public ArrayList<Window> windows = new ArrayList<>();
-    public ArrayList<ScrollBar> scrollBars = new ArrayList<>();
-    public ArrayList<Button> buttons = new ArrayList<>();
-    private WidgetFactory factory = new MotifFactory();
-
-    public void addWindow(String name) {
-        windows.add(factory.createWindow(name));
+    public boolean checkUserExist(String name){
+        boolean exist = false;
+        for(User u: users){
+            if(name.equals(u.name)){
+                exist = true;
+            }
+        }
+        return exist;
     }
 
-    public void addScrollBar(String name) {
-        scrollBars.add(factory.createScrollBar(name));
+    public void addUser(String userType, String userName){
+        if(checkUserExist(name)){
+            System.out.println("Error");
+            return;
+        }
+        user = new User(userType, userName);
+        users.put(type, user);
+        return;
     }
 
-    public void addButton(String name) {
-        buttons.add(factory.createButton(name));
+    public void addBook(String userName, String author, String subject){
+        if(checkUserExist(name)){
+            System.out.println("Error");
+            return;
+        }
+        users.get(userName)
     }
-
-    public void setStyle(String style) {
-        if (style.equals("Motif")) {
-            this.factory = new MotifFactory();
-        } else if (style.equals("PM")) {
-            this.factory = new PMFactory();
-        }
-
-        // update all widgets
-        for (int i = 0; i < windows.size(); i++) {
-            String name = windows.get(i).getName();
-            windows.set(i, factory.createWindow(name));
-        }
-        for (int i = 0; i < scrollBars.size(); i++) {
-            String name = scrollBars.get(i).getName();
-            scrollBars.set(i, factory.createScrollBar(name));
-        }
-        for (int i = 0; i < buttons.size(); i++) {
-            String name = buttons.get(i).getName();
-            buttons.set(i, factory.createButton(name));
-        }
-    }
-
-    public void present() {
-        for (int i = 0; i < windows.size(); i++) {
-            windows.get(i).display();
-        }
-        for (int i = 0; i < scrollBars.size(); i++) {
-            scrollBars.get(i).display();
-        }
-        for (int i = 0; i < buttons.size(); i++) {
-            buttons.get(i).display();
-        }
-    }
-
 }
