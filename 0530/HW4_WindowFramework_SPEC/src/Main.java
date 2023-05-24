fcdd336ffe98739e56dea2a2ab04fdd280b1b29a
R10725045 <r10725045@ntu.edu.tw>
2023-05-24 21:57:20 +0800

    0530 init

---
+++ 0530/HW4_WindowFramework_SPEC/src/Main.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Application application = new Application();
+        ArrayList<String> outputString = new ArrayList<>();
+
+        try {
+            InputParser input = new InputParser();
+            BufferedReader fileReader = input.parse(args);
+            String line = fileReader.readLine();
+
+            while (line != null) {
+                String[] tokens = line.split(" ");
+                String command = tokens[0];
+                switch (command) {
+                    case "PM", "Motif":
+                        application.Styling(command);
+                        break;
+                    case "Button", "ScrollBar", "Window":
+                        application.setWidget(command, tokens[1]);
+                        break;
+                    case "Present":
+                        application.present();
+                        break;
+                    default:
+                        break;
+                }
+                line = fileReader.readLine();
+            }
+            fileReader.close();
+
+        } catch (IOException ex) {
+//            System.out.println("Input Error");
+        }
+        //print
+        for (String line : outputString) {
+            System.out.println(line);
+        }
+    }
+}
