ba36c5ee8c9680e70b1a7a164f07a090c7f2f17f
R10725045 <r10725045@ntu.edu.tw>
2023-05-28 10:53:53 +0800

    add hw2 main

--- 0530/HW2_SchematicCaptureSystems_SPEC/src/InputParser.java
+++ 0530/HW2_SchematicCaptureSystems_SPEC/src/InputParser.java
@@ -2,6 +2,15 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
 public class InputParser {
     public BufferedReader parse(String[] args) throws IOException {
@@ -9,5 +18,24 @@ public class InputParser {
         File fakeDataFile = new File(filename);
         BufferedReader fileReader = new BufferedReader(new FileReader(fakeDataFile));
         return fileReader;
+    }
+
+    public NodeList xmlParse(String[] args) {
+        //parse xml file
+        try {
+            File inputFile = new File(args[0]);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+            NodeList nList = doc.getDocumentElement().getChildNodes();
+            return nList;
+        } catch (ParserConfigurationException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (SAXException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- 0530/HW2_SchematicCaptureSystems_SPEC/src/Main.java
+++ 0530/HW2_SchematicCaptureSystems_SPEC/src/Main.java
@@ -1,43 +1,79 @@
-import java.io.*;
-import java.util.*;
-
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Application application = new Application();
-        ArrayList<String> outputString = new ArrayList<>();
-
+    public static void main(String[] args) {
         try {
             InputParser input = new InputParser();
-            BufferedReader fileReader = input.parse(args);
-            String line = fileReader.readLine();
-
-            while (line != null) {
-                String[] tokens = line.split(" ");
-                String command = tokens[0];
-                switch (command) {
-                    case "PM", "Motif":
-                        application.Styling(command);
+            NodeList childList = input.xmlParse(args);
+            //loop childList
+            for (int index = 0; index < childList.getLength(); index++) {
+                Node node = childList.item(index);
+                String componentType = node.getNodeName();
+                if (componentType.equals("#text")) {
+                    continue;
+                }
+//                System.out.println(componentType);
+                switch (componentType) {
+                    case "Line":
+                        BasicComponent lineComponent  = new Line();
+                        lineComponent.draw();
                         break;
-                    case "Button", "ScrollBar", "Window":
-                        application.setWidget(command, tokens[1]);
+                    case "Rectangle":
+                        BasicComponent rectangle = new Rectangle();
+                        rectangle.draw();
                         break;
-                    case "Present":
-                        application.present();
+                    case "Text":
+                        BasicComponent text = new Text();
+                        text.draw();
                         break;
+                    case "Group":
+                        BasicComponent group = new Group();
+                        group.add();
                     default:
                         break;
-                }
-                line = fileReader.readLine();
             }
-            fileReader.close();
+//            BufferedReader fileReader = input.parse(args);
+//            String line = fileReader.readLine();
 
-        } catch (IOException ex) {
+//            while (line != null) {
+//                String[] tokens = line.split(" ");
+//                String command = tokens[0];
+//                switch (command) {
+//                    case "<Line/>":
+//                        BasicComponent lineComponent  = new Line();
+//                        lineComponent.draw();
+//                        break;
+//                    case "<Rectangle/>":
+//                        BasicComponent rectangle = new Rectangle();
+//                        rectangle.draw();
+//                        break;
+//                    case "<Text/>":
+//                        BasicComponent textBox = new Text();
+//                        text.draw();
+//                        break;
+//                    case "<Group>":
+//                        BasicComponent group = new Group();
+//                        group.add();
+
+//                    case "Button", "ScrollBar", "Window":
+//                        application.setWidget(command, tokens[1]);
+//                        break;
+//                    case "Present":
+//                        application.present();
+//                        break;
+//                    default:
+//                        break;
+//                }
+//                line = fileReader.readLine();
+//            }
+//            fileReader.close();
+
+        } catch (Exception e) {
 //            System.out.println("Input Error");
-        }
-        //print
-        for (String line : outputString) {
-            System.out.println(line);
         }
     }
 }
