17a4a4fc63475eb57dfae9ee67535e62d9c4c3a2
BOBO-LU <luwenkai0820@gmail.com>
2023-05-28 11:56:21 +0800

    add main

--- 0530/HW4_WindowFramework_SPEC/src/Main.java
+++ 0530/HW4_WindowFramework_SPEC/src/Main.java
@@ -4,26 +4,53 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Application application = new Application();
-        ArrayList<String> outputString = new ArrayList<>();
-
+        /*
+        * window [WindowSubtype] [ImplementType]
+            drawBorder
+            drawCloseBox
+        * */
         try {
             InputParser input = new InputParser();
             BufferedReader fileReader = input.parse(args);
             String line = fileReader.readLine();
 
+            String windowsubtype = "";
+            String implementtype = "";
+            WindowSubtype ws = null;
+
             while (line != null) {
                 String[] tokens = line.split(" ");
                 String command = tokens[0];
                 switch (command) {
-                    case "PM", "Motif":
-                        application.Styling(command);
+                    case "window":
+                        windowsubtype = tokens[1];
+                        implementtype = tokens[2];
+                        if (windowsubtype.equals("IconWindow")) {
+                            ws = WindowSubtype(implementtype);
+                            break;
+                        }
+                        if (implementtype.equals("TransientWindow")) {
+                            ws = WindowSubtype(implementtype);
+                            break;
+                        }
                         break;
-                    case "Button", "ScrollBar", "Window":
-                        application.setWidget(command, tokens[1]);
+                    case "drawBorder":
+                        if (ws == null) {
+                            break;
+                        }
+                        if (windowsubtype.equals("IconWindow")) {
+                            ws.drawBorder();
+                            break;
+                        }
                         break;
-                    case "Present":
-                        application.present();
+                    case "drawCloseBox":
+                        if (ws == null) {
+                            break;
+                        }
+                        if (windowsubtype.equals("TransientWindow")) {
+                            ws.drawCloseBox();
+                            break;
+                        }
                         break;
                     default:
                         break;
@@ -34,10 +61,6 @@ public class Main {
 
         } catch (IOException ex) {
 //            System.out.println("Input Error");
-        }
-        //print
-        for (String line : outputString) {
-            System.out.println(line);
         }
     }
 }
