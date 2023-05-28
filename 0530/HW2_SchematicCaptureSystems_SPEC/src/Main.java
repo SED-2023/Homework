be789b6cf8d299f648de2651488e33f7e545383a
R10725045 <r10725045@ntu.edu.tw>
2023-05-28 11:32:01 +0800

    add switch func

--- 0530/HW2_SchematicCaptureSystems_SPEC/src/Main.java
+++ 0530/HW2_SchematicCaptureSystems_SPEC/src/Main.java
@@ -5,6 +5,44 @@ import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
 public class Main {
+
+    private static BasicComponent switchComponents(String componentType, Node node) {
+        if (componentType.equals("#text")) return null;
+        switch (componentType) {
+            case "Line":
+                BasicComponent lineComponent = new Line();
+                lineComponent.draw();
+                System.out.println();
+                return lineComponent;
+            case "Rectangle":
+                BasicComponent rectangle = new Rectangle();
+                rectangle.draw();
+                System.out.println();
+                return rectangle;
+            case "Text":
+                BasicComponent text = new Text();
+                text.draw();
+                System.out.println();
+                return text;
+            case "Group":
+                BasicComponent group = new Group();
+                NodeList groupChildList = node.getChildNodes();
+//                System.out.println(chileComponentType);
+                for (int index = 0; index < groupChildList.getLength(); index++) {
+                    Node childNode = groupChildList.item(index);
+                    String chileComponentType = node.getNodeName();
+//                    System.out.println(chileComponentType);
+                    BasicComponent bc = switchComponents(chileComponentType, childNode);
+                    ((Group) group).add(bc);
+                }
+//                group.draw();
+                return group;
+            default:
+                break;
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
         try {
             InputParser input = new InputParser();
@@ -13,32 +51,32 @@ public class Main {
             for (int index = 0; index < childList.getLength(); index++) {
                 Node node = childList.item(index);
                 String componentType = node.getNodeName();
-                if (componentType.equals("#text")) {
-                    continue;
-                }
+                switchComponents(componentType, node);
 //                System.out.println(componentType);
-                switch (componentType) {
-                    case "Line":
-                        BasicComponent lineComponent = new Line();
-                        lineComponent.draw();
-                        System.out.println();
-                        break;
-                    case "Rectangle":
-                        BasicComponent rectangle = new Rectangle();
-                        rectangle.draw();
-                        System.out.println();
-                        break;
-                    case "Text":
-                        BasicComponent text = new Text();
-                        text.draw();
-                        System.out.println();
-                        break;
-                    case "Group":
-                        BasicComponent group = new Group();
-//                        group.add();
-                    default:
-                        break;
-                }
+//                switch (componentType) {
+//                    case "Line":
+//                        BasicComponent lineComponent = new Line();
+//                        lineComponent.draw();
+//                        System.out.println();
+//                        break;
+//                    case "Rectangle":
+//                        BasicComponent rectangle = new Rectangle();
+//                        rectangle.draw();
+//                        System.out.println();
+//                        break;
+//                    case "Text":
+//                        BasicComponent text = new Text();
+//                        text.draw();
+//                        System.out.println();
+//                        break;
+//                    case "Group":
+//                        BasicComponent group = new Group();
+//                        NodeList groupChildList = node.getChildNodes();
+//
+////                        group.add();
+//                    default:
+//                        break;
+//                }
             }
         } catch (Exception e) {
 //            System.out.println("Input Error");
