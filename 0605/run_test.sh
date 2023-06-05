28815731581a935cf30e39123a9a05efd84676e4
ltsai471 <a221061493@gmail.com>
2023-06-05 14:54:12 +0800

    0605 init

---
+++ 0605/run_test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+javac -cp "lib/*" -d target ./src/*.java
+cd target
+java Main ../sampleInput > ../outputFiles/sampleResult
+
+directory="../testCase"
+search_string="_Input"
+replacement_string="_Result"
+for file in "$directory"/*; do
+    # Check if the current item is a file
+    if [[ -f "$file" ]]; then
+        filename=$(basename "$file")
+        if [[ $filename == *_Input.txt ]]; then
+          echo "Processing file: $filename"
+          result="${filename//$search_string/$replacement_string}"
+          java LibrarySystem ../testCase/"$filename" > ../outputFiles/"$result"
+        fi
+    fi
+done
+
+# run junit test
+java -jar ../lib/junit-platform-console-standalone-1.9.1.jar -cp . --scan-class-path
+
+echo "Press any key to exit..."
+read -n 1 -s
---
+++ template/generate_submit_files.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+folder_path="submit"
+team_name="Team4"
+if [ ! -d "$folder_path" ]; then
+    mkdir -p "$folder_path"
+    cd $folder_path
+    if [ ! -d "$team_name" ]; then
+        mkdir -p "$team_name"
+    fi
+fi
+
+cd ..
+cp src/* "$folder_path"/"$team_name"/
+rm "$folder_path"/"$team_name"/MainTest.java
+
+javac -d target ./"$folder_path"/"$team_name"/*.java
+cd target
+java Main ../sampleInput > ../outputFiles/sampleOutput
