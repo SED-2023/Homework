8d6a27dd1021905ca08f56a154ac26feb8c505f8
ltsai471 <a221061493@gmail.com>
2023-06-06 06:58:07 +0800

    update 心得

--- finalExam/src/Main.java
+++ finalExam/src/Main.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class LibrarySystem {
+public class Main {
     public static void main(String[] args) throws IOException {
         Application application = new Application();
 
