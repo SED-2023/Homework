688cddd39bf8fcaa74b9ae23a78ef3c6e37918d4
imaoplab <103180018+imaoplab@users.noreply.github.com>
2023-06-05 18:32:49 +0800

    last

--- 0605/src/LibrarySystem.java
+++ 0605/src/LibrarySystem.java
@@ -18,63 +18,63 @@ public class LibrarySystem {
                 String[] tokens = line.split(" ");
                 application.initAddBook(tokens[0], tokens[1]);
             }
-            // add init users
+//             add init users
             line = fileReader.readLine();
             int numOfUsers = Integer.parseInt(line);
             for (int i = 0; i < numOfUsers; i++) {
                 line = fileReader.readLine();
                 String[] tokens = line.split(" ");
                 if (tokens[0].equals("Staff") && tokens.length == 2)
-                    application.initAddUser(tokens[0], tokens[1], "0");
+                    application.initAddUser(tokens[0], tokens[1], 0);
                 else if (tokens[0].equals("Borrower") && tokens.length == 3)
-                    application.initAddUser(tokens[0], tokens[1], tokens[2]);
+                    application.initAddUser(tokens[0], tokens[1], Integer.parseInt(tokens[2]));
                 else {
                     System.out.println("Error");
                 }
             }
             // do functions
-            line = fileReader.readLine();
-            while (line != null) {
-                String[] tokens = line.split(" ");
-                String user = tokens[0];
-                String command = tokens[1];
-                switch (command) {
-                    case "addBook":
-                        line = fileReader.readLine();
-                        tokens = line.split(" ");
-                        application.addBook(user, tokens[0], tokens[1]);
-                        break;
-                    case "removeBook":
-                        application.removeBook(user, Integer.parseInt(tokens[2]));
-                        break;
-                    case "checkout":
-                        String user2 = tokens[2];
-                        line = fileReader.readLine();
-                        tokens = line.split(" ");
-
-                        ArrayList<String> stringList = new ArrayList<>(Arrays.asList(tokens));
-                        application.checkOut(user, user2, stringList);
-                        break;
-                    case "returnBook":
-                        application.returnBook(user, Integer.parseInt(tokens[2]));
-                        break;
-                    case "listAuthor":
-                        application.listAuthor(user, tokens[2]);
-                        break;
-                    case "listSubject":
-                        application.listSubject(user, tokens[2]);
-                        break;
-                    case "findChecked":
-                        application.findChecked(user, tokens[2]);
-                        break;
-                    case "listBorrower":
-                        application.listBorrower(user, Integer.parseInt(tokens[2]));
-                        break;
-                    default:
-                        break;
-                }
-                line = fileReader.readLine();
-            }
+//            line = fileReader.readLine();
+//            while (line != null) {
+//                String[] tokens = line.split(" ");
+//                String user = tokens[0];
+//                String command = tokens[1];
+//                switch (command) {
+//                    case "addBook":
+//                        line = fileReader.readLine();
+//                        tokens = line.split(" ");
+//                        application.addBook(user, tokens[0], tokens[1]);
+//                        break;
+//                    case "removeBook":
+//                        application.removeBook(user, Integer.parseInt(tokens[2]));
+//                        break;
+//                    case "checkout":
+//                        String user2 = tokens[2];
+//                        line = fileReader.readLine();
+//                        tokens = line.split(" ");
+//
+//                        ArrayList<String> stringList = new ArrayList<>(Arrays.asList(tokens));
+//                        application.checkOut(user, user2, stringList);
+//                        break;
+//                    case "returnBook":
+//                        application.returnBook(user, Integer.parseInt(tokens[2]));
+//                        break;
+//                    case "listAuthor":
+//                        application.listAuthor(user, tokens[2]);
+//                        break;
+//                    case "listSubject":
+//                        application.listSubject(user, tokens[2]);
+//                        break;
+//                    case "findChecked":
+//                        application.findChecked(user, tokens[2]);
+//                        break;
+//                    case "listBorrower":
+//                        application.listBorrower(user, Integer.parseInt(tokens[2]));
+//                        break;
+//                    default:
+//                        break;
+//                }
+//                line = fileReader.readLine();
+//            }
             fileReader.close();
 
         } catch (IOException ex) {
--- 0605/src/User.java
+++ 0605/src/User.java
@@ -1,5 +1,17 @@
 public abstract class User {
-    String name;
-    String type;
-    int preDefinedNumber;
+    private String name;
+    private String type;
+
+    public User(String name, String type){
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
