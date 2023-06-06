a24a38826ce92f05a3b337dd806ef03c5c70a062
ltsai471 <a221061493@gmail.com>
2023-06-06 12:35:45 +0800

    add 3 assignment func

--- finalExam/src/Assignment.java
+++ finalExam/src/Assignment.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Assignment {
     private Student author;
@@ -14,16 +16,62 @@ public class Assignment {
         reviews.put(review.getStudentId(), review);
     }
 
-    public void calculateScore(String homeworkId, String studentId, String rankingStrategy) {
-        // TODO: 琳
+    public void calculateScore(ArrayList<Criterion> criterionList, String homeworkId, String studentId, RankingStrategy rankingStrategy) {
+        double sum = 0;
+        double count = criterionList.size();
+        for (Criterion c : criterionList) {
+            ArrayList<Integer> scoreList = new ArrayList<>();
+            //loop through all reviews
+            for (Map.Entry<String, Review> entry : reviews.entrySet()) {
+                String author = entry.getKey();
+                Review review = entry.getValue();
+                scoreList.add(review.getContent().get(c).getScore());
+            }
+            sum += rankingStrategy.calculate(scoreList);
+        }
+        System.out.printf("Assignment: %s, Student: %s, Score: %f\n", homeworkId, studentId, Math.round(sum / count));
     }
 
-    public void findStrength(String homeworkId, String studentId, String rankingStrategy) {
-        // TODO: 琳
+    public void findStrength(ArrayList<Criterion> criterionList, String homeworkId, String studentId, RankingStrategy rankingStrategy) {
+        double maxScore = 0;
+        String strength = "";
+        for (Criterion c : criterionList) {
+            ArrayList<Integer> scoreList = new ArrayList<>();
+            for (Map.Entry<String, Review> entry : reviews.entrySet()) {
+                String author = entry.getKey();
+                Review review = entry.getValue();
+                scoreList.add(review.getContent().get(c).getScore());
+            }
+            double calScore = rankingStrategy.calculate(scoreList);
+            if (calScore > maxScore) {
+                maxScore = calScore;
+                strength = c.getName();
+            } else if (calScore == maxScore) {
+                strength += ' ' + c.getName(); // need a space
+            }
+        }
+        System.out.printf("Assignment: %s, Student: %s, Strength:%s)", homeworkId, studentId, strength);
     }
 
-    public void findWeakness(String homeworkId, String studentId, String rankingStrategy) {
-        // TODO: 琳
+    public void findWeakness(ArrayList<Criterion> criterionList, String homeworkId, String studentId, RankingStrategy rankingStrategy) {
+        double minScore = 0;
+        String weakness = "";
+        for (Criterion c : criterionList) {
+            ArrayList<Integer> scoreList = new ArrayList<>();
+            for (Map.Entry<String, Review> entry : reviews.entrySet()) {
+                String author = entry.getKey();
+                Review review = entry.getValue();
+                scoreList.add(review.getContent().get(c).getScore());
+            }
+            double calScore = rankingStrategy.calculate(scoreList);
+            if (calScore < minScore) {
+                minScore = calScore;
+                weakness = c.getName();
+            } else if (calScore == minScore) {
+                weakness += ' ' + c.getName(); // need a space
+            }
+        }
+        System.out.printf("Assignment: %s, Student: %s, Weakness:%s)", homeworkId, studentId, weakness);
     }
 
     // getter and setter
