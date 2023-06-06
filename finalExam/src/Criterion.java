8dbd0c201346ad263fe8b86ec80c11543e1ef18b
BOBO-LU <luwenkai0820@gmail.com>
2023-06-06 11:39:48 +0800

    RubricItem, Criterion, Level

---
+++ finalExam/src/Criterion.java
@@ -0,0 +1,11 @@
+public class Criterion {
+    private String name;
+
+    public Criterion(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
